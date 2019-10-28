df0cd2f0c450a56a327a71a48f8995e1f1dfea54
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-10-28 17:26:51 +0400

    Revert "Restrict user_list and user_show access (GDPR)"

--- ckanext/inverness/plugin.py
+++ ckanext/inverness/plugin.py
@@ -1,29 +1,13 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-import ckanext.inverness.helpers as h
 
 
 class InvernessPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IActions)
-    plugins.implements(plugins.IRoutes, inherit=True)
 
     # IConfigurer
+
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'inverness')
-
-    # IActions
-    def get_actions(self):
-        module_root = 'ckanext.inverness.logic.action'
-        action_functions = h._get_functions(module_root)
-        return action_functions
-
-    # IRoutes
-    def before_map(self, map):
-        # Handle user index route to check if the user is sysadmin
-        user_ctrl = 'ckanext.inverness.controllers.inverness:InvernessController'
-        map.connect('user_index', '/user',
-                    controller=user_ctrl, action='index')
-        return map
